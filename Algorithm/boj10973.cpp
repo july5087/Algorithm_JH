cb941bb3b1702fcf59fdc3fd1b6450606cf84eec
JooHyun <july5087@gmail.com>
2020-02-18 22:07:44 +0900

    [boj_10973]이전 순열

--- Algorithm/boj10973.cpp
+++ Algorithm/boj10973.cpp
@@ -1,4 +1,4 @@
-//다음 순열
+//이전 순열
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -7,26 +7,25 @@ int N;
 int permu[10000];
 
 void solution() {
-	if (next_permutation(permu, permu + N) == true) {
-		for (int i = 0; i < N; i++) {
+	if (prev_permutation(permu, permu + N) == true) {
+		for (int i = 0; i < N; i++)
 			cout << permu[i] << " ";
-		}
 		cout << "\n";
 	}
 	else
 		cout << "-1\n";
 }
 
-int main() 
-{
+int main() {
+
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
 	cin >> N;
-	
+
 	for (int i = 0; i < N; i++)
 		cin >> permu[i];
-	
+
 	solution();
 }
