b69d41a1210d8c1f7fc913edcd9d7b3deb6eca2a
JooHyun <july5087@gmail.com>
2020-03-13 22:11:17 +0900

    [programmers12901]2016년

---
+++ Algorithm/programmers12901.cpp
@@ -0,0 +1,17 @@
+//2016³â
+#include<iostream>
+#include<string>
+using namespace std;
+
+string solution(int a, int b)
+{
+	string day[] = { "THU","FRI","SAT","SUN","MON","TUE","WED" };
+	int month[] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
+	int sum = 0;
+
+	for (int i = 0; i<a - 1; i++) {
+		sum += month[i];
+	}
+	sum += b;
+	return day[sum % 7];
+}
