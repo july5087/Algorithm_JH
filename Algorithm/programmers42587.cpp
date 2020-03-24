f90f96a7cfe51fbefbd0efe83776cf80b2a56ede
JooHyun <july5087@gmail.com>
2020-03-25 03:39:41 +0900

    [programmers42587]프린터

---
+++ Algorithm/programmers42587.cpp
@@ -0,0 +1,35 @@
+//«¡∏∞≈Õ
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+	int answer = 0;
+
+	queue<pair<int, int>> que;
+	priority_queue<int> que2;
+	for (int i = 0; i < priorities.size(); i++) {
+		que.push(make_pair(i, priorities[i]));
+		que2.push(priorities[i]);
+	}
+
+	while (!que.empty()) {
+		if (que.front().second == que2.top()) {
+			if (que.front().first == location) {
+				return answer + 1;
+			}
+			else {
+				answer++;
+				que.pop(); que2.pop();
+			}
+		}
+		else {
+			que.push(que.front());
+			que.pop();
+		}
+	}
+	return answer;
+}
